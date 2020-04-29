72ffb5f1fb28821826ec59a7cf510132c96c11dc
MedleysMind <patrickjmedley@gmail.com>
2020-04-29 17:00:53 -0400

    video is functional

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
 var config = {
-    type: Phaser.AUTO,
-    width: 400,
-    height: 800,
-    
-    scene: {
-        preload: preload,
-        create: create
-    },
+  type: Phaser.AUTO,
+  width: 450,
+  height: 800,
+  scale: {
+    // parent: "div",
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  scene: {
+    preload: preload,
+    create: create,
+  },
 };
 
 var game = new Phaser.Game(config);
 
-function preload ()
-{
-
-    this.load.video('intro', '/assets/videos/TITLEMOBILE.mp4', 'loadeddata', false, false);
+function preload() {
+  this.load.video(
+    "intro",
+    "/assets/videos/TITLEMOBILE.mp4",
+    "loadeddata",
+    false,
+    false
+  );
+  
 }
 
-function create ()
-{
-    var vid = this.add.video(200, 400, 'intro');
-
-        // vid.play(true);
+function create() {
+    
+  var vid = this.add.video(225 , 400, "intro");
+  vid.setScale(.5);
+//   vid.displayHeight = 800;
+  // var played = false;
+//   if (!played) {
+    vid.play(true);
+    // played = true;
+//   }
 }
